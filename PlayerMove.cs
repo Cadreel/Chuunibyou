cbd21945d7f962fef50e463d5c948379043c1cd7
Gabriel Faggione <gabriel.faggione@gmail.com>
2019-05-16 00:55:55 -0300

    Adicionado scripts básicos

---
+++ PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal") / speed;
+        float moveVertical = Input.GetAxis ("Vertical") / speed;
+
+        Debug.Log(moveHorizontal);
+        Debug.Log(moveVertical);
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        transform.position += movement;
+        //rb.AddForce (movement * speed);
+    }
+}
